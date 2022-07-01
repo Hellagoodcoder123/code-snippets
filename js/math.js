baadfa47ef08b12eab8681d59c101668485655eb
Hellagoodcoder123 <95957618+Hellagoodcoder123@users.noreply.github.com>
2022-07-01 13:00:17 +1000

    Update math.js

--- js/math.js
+++ js/math.js
@@ -1,7 +1,7 @@
 var math = {
   abs:function(x){if(`${x}`.split('-')[1]===undefined){return x}else{return parseInt(`${x}`.split('-')[1])}},
   floor:function(x){return parseInt(`${x}`.split('.')[0])},
-  ceil:function(x){if(`${x}`.split('.')[1]===undefined){return x}else{return parseInt(`${x}`.split('.')[0])+1}}
+  ceil:function(x){if(`${x}`.split('.')[1]===undefined){return x}else{return parseInt(`${x}`.split('.')[0])+1}},
   divisibleBy:function(value,divisor){return value/divisor==math.floor(value/divisor)},
   isPrime:function(check){return !(math.divisibleBy(check,2)||math.divisibleBy(check,3)||math.divisibleBy(check,5)||math.divisibleBy(check,7))},
   isSquare:function(check){return Math.sqrt(check)==math.floor(Math.sqrt(check))},
